dac032e81adcb75e019f5c14ff75ee7dada400f0
inglembomarco@gmail.com <marcolembo@192.168.0.11>
2015-03-23 14:04:16 +0100

    ultime modifiche su stili, fullimage, etc...

--- cache/template/yoo_stage.php
+++ cache/template/yoo_stage.php
@@ -1,6 +1,6 @@
 {
-    "tmpl_check": "238a4690467564dec33a1822a86abde8",
+    "tmpl_check": "c6f12f5899a8d257f1755ed0d64363c7",
     "tmpl_data": "{\"status\":\"update-available\",\"message\":\"New version available! <a href=\\\"http:\\\/\\\/www.yootheme.com\\\/themes\\\/downloads\\\" target=\\\"_blank\\\">Download Stage Theme 1.0.2<\\\/a>\"}",
-    "warp_check": "55a444850ea921c95f9014e55c4be5a1",
+    "warp_check": "623c6d388d76e2754b385adcd84da837",
     "warp_data": "{\"status\":\"up-to-date\",\"message\":\"You're up to date!. 7.3.1 is currently the newest version available.\"}"
 }
